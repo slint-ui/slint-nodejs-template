18a503ea16130edc80e1b167234a5ed820832c5a
Enyium <123484196+Enyium@users.noreply.github.com>
2024-09-19 11:25:50 +0200

    Fixed kebab casing of previous `appwindow.slint`.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-
 import * as slint from "slint-ui";
 
-let ui = slint.loadFile("ui/appwindow.slint");
+let ui = slint.loadFile("ui/app-window.slint");
 let window = new ui.AppWindow();
 
 window.request_increase_value = function () {
